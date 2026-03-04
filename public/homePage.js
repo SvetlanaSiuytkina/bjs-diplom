77cb4bccd06f15b19dc7240b7298be24eb1ba48e
SVETA <svetadan1234@mail.ru>
2026-03-04 14:03:42 +0500

    Требует доработки

--- public/homePage.js
+++ public/homePage.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { response } = require("express");
+
 const logoutButton = new LogoutButton();
 const ratesBoard = new RatesBoard();
 const moneyManager = new MoneyManager();
@@ -15,19 +17,83 @@ logoutButton.action = function() {
 
 ApiConnector.current(response => {
         if (response.success) {
-            ProfileWidget.showProfile(response);
+            ProfileWidget.showProfile(response.data);
         }
     });
 
-    //function getExchangeRate() {
-        //ApiConnector.______(response => {
+    function getExchangeRate() {
+        ApiConnector.getStocks(response => {
         if (response.success) {
             ratesBoard.clearTable();
-            ratesBoard.fillTable(response);
+            ratesBoard.fillTable(response.data);
         }
     });
 }
 
 getExchangeRate();
-setInterval(getExchangeRate(), 60000);
+setInterval(getExchangeRate, 60000);
 
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success);
+        } else {
+            moneyManager.setMessage(response.error);
+        }
+    });
+}
+
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success);
+        } else {
+            moneyManager.setMessage(response.error);
+        }
+    });
+}
+
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success);
+        } else {
+            moneyManager.setMessage(response.error);
+        }
+    });
+}
+
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        updateUsersList();
+    }
+});
+
+favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            updateUsersList();
+        } else {
+            favoritesWidget.setMessage(response.error);
+        }
+    });
+}
+
+favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            updateUsersList();
+        } else {
+            favoritesWidget.setMessage(response.error);
+        }
+    });
+}
